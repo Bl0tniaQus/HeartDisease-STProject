c4824fbd0914d1e7518e6ecaeb57d30a8275e04b
Bl0tniaQus <focusedmud@wp.pl>
2023-01-19 23:47:53 +0100

    zmieniono:       main.py 	zmieniono nazwę: templates/style.css -> static/style.css 	zmieniono:       templates/index.html 	zmieniono:       templates/logowanie.html

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+    
+@app.route('/login')
+def login():
+    return render_template("logowanie.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
