360ce5b16ab6ba096b38d251a26ce2f58b6bc6d6
Bl0tniaQus <focusedmud@wp.pl>
2023-01-21 00:55:08 +0100

    zmieniono:       classifierSimplified.py 	zmieniono:       main.py 	nowy plik:       static/boundssmp.ptk 	zmieniono:       static/network_simplified.ptk 	zmieniono:       templates/formularz.html 	usunięto:        templates/style.css 	nowy plik:       templates/wynik.html 	usunięto:        test.py

--- main.py
+++ main.py
@@ -32,11 +32,7 @@ def logowanie():
 	return render_template("logowanie.html")
 @app.route("/form")
 def formularz():
-	wynik=""
-	if 'wynik' in session:
-		wynik=session['wynik']
-		session.pop('wynik')
-	return render_template("formularz.html",wynik=wynik)
+	return render_template("formularz.html")
 		
 @app.route('/logowanie_action', methods=["POST"])
 def logowanie_action():
@@ -105,33 +101,199 @@ def wyloguj():
 def form_extended():
 	if request.method == "POST":
 		dane = [None]*12
-		dane[0] = normalizuj(18,80,request.form["wiek"])
-		dane[1] = normalizuj(0,1,request.form["plec"])
-		dane[2] = normalizuj(0,4,request.form["bol"])
-		dane[3] = normalizuj(90,200,request.form["cisnienie"])
-		dane[4] = normalizuj(120,600,request.form["cholesterol"])
-		dane[5] = normalizuj(1,2,request.form["cukier"])
-		dane[6] = normalizuj(0,2,request.form["ekg"])
-		dane[7] = normalizuj(70,210,request.form["bicieserca"])
-		dane[8] = normalizuj(0,1,request.form["bolw"])
-		dane[9] = normalizuj(0,6.5,request.form["st"])
-		dane[10] = normalizuj(1,3,request.form["nachylenie"])
-		dane[11] = normalizuj(3,7,request.form["talasemia"])
+		err=False
+		msg = ""
+		wynik="Brak"
+		test = "rozszerzony"
 		
-		network = joblib.load('static/network_extended.ptk')
-		bounds = joblib.load('static/boundsext.ptk')
+		if request.form['plec'] == "0":
+			plec = "Kobieta"
+		else:
+			plec = "Mężczyzna"
+			
+		if request.form['bol']=="0":
+			bol = "Brak"
+		elif request.form['bol']=="1":
+			bol = "1. "
+		elif request.form['bol']=="2":
+			bol = "2. "
+		elif request.form['bol']=="3":
+			bol = "3. "
+		elif request.form['bol']=="4":
+			bol = "4. "
+			
+		if request.form['cukier']=="1":
+			cukier = ">120mg/dl"
+		else:
+			cukier = "<120mg/dl"
 		
-		result = normalizuj(bounds[0],bounds[1],network.predict([dane]),0,1)
-		if result>=0 and result<25:
-			wynik = "Niskie"
-		if result>=25 and result<50:
-			wynik = "Podwyższone"
-		if result>=50 and result<75:
-			wynik = "Duże"
-		if result>=75 and result<=100:
-			wynik = "Bardzo duże"
-		session['wynik'] = wynik
+		if request.form['ekg']=="0":
+			ekg = "W normie"
+		elif request.form['ekg']=="1":
+			ekg = "Anomalie fali ST-T"
+		else:
+			ekg= "Hipertrofia"
+		
+		if request.form['bolw'] == "0":
+			bolw = "Brak"
+		else:
+			bolw = "Występuje"
+			
+		if request.form['nachylenie']=="1":
+			nachylenie = "W górę"
+		elif request.form['nachylenie']=="2":
+			nachylenie = "Płasko"
+		else:
+			nachylenie = "W dół"
+		
+		if request.form['talasemia'] == "3":
+			talasemia = "Brak"
+		elif request.form['talasemia'] == "6":
+			talasemia = "Miejscowa niedokrwistość"
+		else:
+			talasemia = "Ogólna niedokrwistość"
+		
+		
+
+		table = "<tr><td>Wiek: {}</td><td>Płeć: {}</td></tr>".format(request.form['wiek'], plec)
+		table = table + "<tr><td>Ból klatki: {}</td><td>Ból klatki podczas wysiłku: {}</td></tr>".format(bol,bolw)
+		table = table + "<tr><td>Wynik EKG: {}</td><td>Ciśnienie spoczynkowe: {}mmHg</td></tr>".format(ekg,int(request.form['cisnienie']))
+		table = table + "<tr><td>Nachylenie ST: {}</td><td>Cholesterol całkowity: {}mg/dl</td></tr>".format(nachylenie,int(request.form['cholesterol']))
+		table = table + "<tr><td>Różnica ST spoczynek/wysiłek: {}</td><td>Poziom cukru we krwi: {}</td></tr>".format(float(request.form['st']),cukier)
+		table = table + "<tr><td>Talasemia: {}</td><td>Bicie serca (wysiłek): {}bpm</td></tr>".format(talasemia,int(request.form['bicieserca']))
+		if int(request.form["wiek"])<18:
+			msg+="<li>Podany wiek wynosi {} lat. Mając w takim wieku jakiekolwiek wątpliwości co do swojego stanu zdrowia zdecydowanie nie powinno się ich ignorować</li>".format(int(request.form["wiek"]))
+			err=True
+		if int(request.form["wiek"])>80:
+			msg+="<li>Podany wiek wynosi {} lat. W takim wieku problemy zdrowotne mogą mieć bardzo dużo różnych źródeł, dlatego otrzymany naszego wynik mógłby być mylący</li>".format(int(request.form["wiek"]))
+			err=True
+		if int(request.form["cisnienie"])<94:
+			msg+="<li>Ciśnienie skurczowe krwi w wysokości {} mmHg jest bardzo niską, niebezpieczną dla zdrowia wartością</li>".format(int(request.form["cisnienie"]))
+			err=True
+		if int(request.form["cisnienie"])>200:
+			msg+="<li>Ciśnienie skurczowe krwi w wysokości {} mmHg jest niebezpieczną wartością, mogącą wskazywać na nadciśnienie</li>".format(int(request.form["cisnienie"]))
+			err=True
+		if int(request.form["cholesterol"])<120:
+			msg+="<li>Poziom całkowitego cholesterolu we krwi wynoszący {} mg/dl jest poniżej wartości normalnej, co może być przejawem różnych chorób, nawet tych nie związanych bezpośrednio z układem krążenia </li>".format(int(request.form["cholesterol"]))
+			err=True
+		if int(request.form["cholesterol"])>600:
+			msg+="<li>Poziom całkowitego cholesterolu we krwi wynoszący {} mg/dl jest skrajnie wysoki i niebezpieczny, który trzeba bezzwłocznie skonsultować</li>".format(int(request.form["cholesterol"]))
+			err=True
+		if int(request.form["bicieserca"])<70:
+			msg+="<li>Tempo bicia serca po wysiłku równe {} uderzeń na minutę jest nienaturalnie niskie nawet wśród osób wysportowanych i może z dużym prawdopodobieństwem wskazywać na problemy z układem krążenia</li>".format(int(request.form["bicieserca"]))	
+			err=True
+		if int(request.form["bicieserca"])>210:
+			msg+="<li>Tempo bicia serca po wysiłku równe {} uderzeń na minutę jest wysokie niezależnie od wieku i zdecydowanie warto się pod tym kątem zdiagnozować u specjalisty</li>".format(int(request.form["bicieserca"]))	
+			err=True
+		if float(request.form["st"])>6.5:
+			msg+="<li>Różnica długości odcinka ST pomiędzy stanem spoczynku a wysiłkiem wynosząca {} jest skrajnie nienaturalna i jest oznaką nieprawidłowej pracy serca</li>".format(float(request.form["st"]))	
+			err=True
+
+		if err==False:
+			dane[0] = normalizuj(18,80,request.form["wiek"])
+			dane[1] = normalizuj(0,1,request.form["plec"])
+			dane[2] = normalizuj(0,4,request.form["bol"])
+			dane[3] = normalizuj(90,200,request.form["cisnienie"])
+			dane[4] = normalizuj(120,600,request.form["cholesterol"])
+			dane[5] = normalizuj(1,2,request.form["cukier"])
+			dane[6] = normalizuj(0,2,request.form["ekg"])
+			dane[7] = normalizuj(70,210,request.form["bicieserca"])
+			dane[8] = normalizuj(0,1,request.form["bolw"])
+			dane[9] = normalizuj(0,6.5,request.form["st"])
+			dane[10] = normalizuj(1,3,request.form["nachylenie"])
+			dane[11] = normalizuj(3,7,request.form["talasemia"])
+			
+			network = joblib.load('static/network_extended.ptk')
+			bounds = joblib.load('static/boundsext.ptk')
+		
+			result = normalizuj(bounds[0],bounds[1],network.predict([dane]),0,1)
+			print(result)
+			if result>=0 and result<0.25:
+				wynik = "Niskie"
+			if result>=0.25 and result<0.50:
+				wynik = "Podwyższone"
+			if result>=0.50 and result<0.75:
+				wynik = "Duże"
+			if result>=0.75 and result<=1:
+				wynik = "Bardzo duże"
+		return render_template("wynik.html", msg=msg, wynik=wynik,test=test, table=table)
+	return redirect("/form")
+	
+@app.route('/form_simplified', methods=["POST"])
+def form_simplified():
+	if request.method == "POST":
+		dane = [None]*8
+		err=False
+		msg = ""
+		wynik="Brak"
+		test = "uproszczony"
+		
+		if request.form['plec'] == "0":
+			plec = "Kobieta"
+		else:
+			plec = "Mężczyzna"
+			
+		if request.form['bol']=="0":
+			bol = "Brak"
+		elif request.form['bol']=="1":
+			bol = "1. "
+		elif request.form['bol']=="2":
+			bol = "2. "
+		elif request.form['bol']=="3":
+			bol = "3. "
+		elif request.form['bol']=="4":
+			bol = "4. "
+			
+		if request.form['cukier']=="1":
+			cukier = ">120mg/dl"
+		else:
+			cukier = "<120mg/dl"
+		
+		if request.form['bolw'] == "0":
+			bolw = "Brak"
+		else:
+			bolw = "Występuje"
+			
+		table = "<tr><td>Wiek: {}</td><td>Płeć: {}</td></tr>".format(request.form['wiek'], plec)
+		table = table + "<tr><td>Ból klatki: {}</td><td>Ból klatki podczas wysiłku: {}</td></tr>".format(bol,bolw)
+		table = table + "<tr><td>Cholesterol całkowity ~{}</td><td>Bicie serca (wysiłek): {}bpm</td></tr>".format(int(request.form['cholesterol']),int(request.form['bicieserca']))
+		table = table + "<tr><td>Poziom cukru we krwi: {}</td><td>Ciśnienie spoczynkowe: ~{}mmHg</td></tr>".format(cukier,int(request.form['cisnienie']))
+		
+		if int(request.form["wiek"])<18:
+			msg+="<li>Podany wiek wynosi {} lat. Mając w takim wieku jakiekolwiek wątpliwości co do swojego stanu zdrowia zdecydowanie nie powinno się ich ignorować</li>".format(int(request.form["wiek"]))
+			err=True
+		if int(request.form["wiek"])>80:
+			msg+="<li>Podany wiek wynosi {} lat. W takim wieku problemy zdrowotne mogą mieć bardzo dużo różnych źródeł, dlatego otrzymany naszego wynik mógłby być mylący</li>".format(int(request.form["wiek"]))
+			err=True
+			
+		
+				
+		if err==False:
+			dane[0] = normalizuj(18,80,request.form["wiek"])
+			dane[1] = normalizuj(0,1,request.form["plec"])
+			dane[2] = normalizuj(0,4,request.form["bol"])
+			dane[3] = normalizuj(90,200,request.form["cisnienie"])
+			dane[4] = normalizuj(120,600,request.form["cholesterol"])
+			dane[5] = normalizuj(1,2,request.form["cukier"])
+			dane[6] = normalizuj(70,210,request.form["bicieserca"])
+			dane[7] = normalizuj(0,1,request.form["bolw"])
+
+			network = joblib.load('static/network_simplified.ptk')
+			bounds = joblib.load('static/boundssmp.ptk')
+		
+			result = normalizuj(bounds[0],bounds[1],network.predict([dane]),0,1)
+			print(result)
+			if result>=0 and result<0.25:
+				wynik = "Niskie"
+			if result>=0.25 and result<0.50:
+				wynik = "Podwyższone"
+			if result>=0.50 and result<0.75:
+				wynik = "Duże"
+			if result>=0.75 and result<=1:
+				wynik = "Bardzo duże"
+		return render_template("wynik.html", msg=msg, wynik=wynik,test=test, table=table)
 	return redirect("/form")	
+		
 	
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++
@@ -1,4 +0,0 @@
-xd = [None]*12
-xd[0] = 1
-xd[1] = 2
-print(xd)
