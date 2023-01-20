54fb574fe8293e62b804c62d413d0a853c3ed44c
Bl0tniaQus <focusedmud@wp.pl>
2023-01-20 11:47:42 +0100

    zmieniono:       main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template
+import psycopg2
+
+def dbConnect():
+    dbConnection = psycopg2.connect(host='localhost',database='heairt',user='postgres',password='postgres')
+    return dbConnection
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+	return render_template("index.html")
     
 @app.route('/login')
 def login():
-    return render_template("logowanie.html")
+	return render_template("logowanie.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
